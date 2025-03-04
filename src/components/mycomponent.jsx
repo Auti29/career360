9fe9b9295a26484f6428e2605ffbadd6f24fc139
achyutauti30@gmail.com <achyutauti30@gmail.com>
2025-03-04 16:49:43 +0530

    added folders

---
+++ src/components/mycomponent.jsx
@@ -0,0 +1,7 @@
+export default function Mycomponent() {
+  return (
+    <div>
+      <h2>component</h2>
+    </div>
+  );
+}
---
+++ src/pages/mypage.jsx
@@ -0,0 +1,7 @@
+export default function Mypage() {
+  return (
+    <div>
+      <h2>page</h2>
+    </div>
+  );
+}
